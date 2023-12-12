3307b6b69a20d76abfb0dd70161bd2e1ed4aab85
Rodrigo Salazar <rodrigosalad@gmail.com>
2023-12-12 03:36:09 -0500

    Init friendship microservice

---
+++ friendship-service/src/main/java/com/tuxedo/friendship_service/FriendshipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tuxedo.friendship_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendshipServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendshipServiceApplication.class, args);
+	}
+
+}
---
+++ friendship-service/src/test/java/com/tuxedo/friendship_service/FriendshipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuxedo.friendship_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendshipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
